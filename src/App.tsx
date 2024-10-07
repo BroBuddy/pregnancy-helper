43690be5508aa5cfa8ea598f0d59d07cd7955f0b
Bro Buddy <sirstone@gmx.de>
2024-10-07 12:35:43 +0200

    fixed router scroll top

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import MobileNav from './components/MobileNav'
+import { useLayoutEffect } from 'react'
 import './App.css'
 
 function App() {
+    const location = useLocation()
+
+    useLayoutEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+    }, [location.pathname])
+
     return (
         <>
             <Outlet />
