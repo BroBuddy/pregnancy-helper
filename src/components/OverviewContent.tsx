ef71eae8f437e649527eecc7a047a7af82235063
Bro Buddy <sirstone@gmx.de>
2024-10-07 12:18:35 +0200

    fixed overview contents

--- src/components/OverviewContent.tsx
+++ src/components/OverviewContent.tsx
@@ -7,34 +7,32 @@ const OverviewContent = ({ items }: { items: any[] }) => {
             {items.map((item: any, index: number) => {
                 return (
                     <React.Fragment key={index}>
-                        {index >= 1 && (
-                            <Link
-                                to={`/week/${index + 1}`}
-                                className="flex flex-col"
-                            >
-                                <strong className="text-white">
-                                    {index + 1}. Woche
-                                </strong>
+                        <Link
+                            to={`/week/${index + 2}`}
+                            className="flex flex-col"
+                        >
+                            <strong className="text-white">
+                                {index + 2}. Woche
+                            </strong>
 
-                                {item.length === 0 && (
-                                    <span className="text-sm text-gray-500">
-                                        Wurde noch nicht gepflegt.
-                                    </span>
-                                )}
+                            {item.length === 0 && (
+                                <span className="text-sm text-gray-500">
+                                    Wurde noch nicht gepflegt.
+                                </span>
+                            )}
 
-                                {item.length >= 1 &&
-                                    item.map((item: any, index: number) => {
-                                        return (
-                                            <span
-                                                className="text-sm text-blue-500"
-                                                key={index}
-                                            >
-                                                {item.name}
-                                            </span>
-                                        )
-                                    })}
-                            </Link>
-                        )}
+                            {item.length >= 1 &&
+                                item.map((item: any, index: number) => {
+                                    return (
+                                        <span
+                                            className="text-sm text-blue-500"
+                                            key={index}
+                                        >
+                                            {item.name}
+                                        </span>
+                                    )
+                                })}
+                        </Link>
                     </React.Fragment>
                 )
             })}
