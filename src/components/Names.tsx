7a82124a726d471243050c53551bcbeb18acc8a1
Bro Buddy <sirstone@gmx.de>
2024-10-07 09:53:07 +0200

    improved names area

--- src/components/Names.tsx
+++ src/components/Names.tsx
@@ -1,44 +1,159 @@
-import { Babynames } from '@/service/names'
+import { BoyNames, GirlNames } from '@/service/names'
+import { Tabs, TabsContent, TabsList } from '@radix-ui/react-tabs'
+import TabButton from './TabButton'
+import NamesList from './NamesList'
 
 export const Names = () => {
-    const names = Babynames
+    const boyNames = BoyNames
+    const girlNames = GirlNames
 
-    const amountOfNames = names.map((name: any) => name.names).flat()
+    const amountOfBoyNames = boyNames.map((name: any) => name.names).flat()
+    const amountOfGirlNames = girlNames.map((name: any) => name.names).flat()
 
     return (
         <section className="flex flex-col py-2 w-80">
-            <div className="card">
-                <h2>Jungennamen</h2>
+            <Tabs defaultValue="gender" className="w-full">
+                <div className="rounded-lg bg-white/70 my-5 p-4">
+                    <TabsList className="flex flex-row mx-2 justify-center">
+                        <TabButton
+                            value="gender"
+                            text="Geschlecht"
+                            width={45}
+                        />
+                        <TabButton value="boy" text="Jungen" width={45} />
+                        <TabButton value="girl" text="Mädchen" width={45} />
+                    </TabsList>
+                </div>
 
-                {amountOfNames && (
-                    <p className="text-sm">
-                        Aktuell stehen {amountOfNames.length} Jungennamen zur
-                        Auswahl.
-                    </p>
-                )}
-            </div>
+                <div className="flex mt-2">
+                    <div className="flex flex-col w-full">
+                        <TabsContent value="gender">
+                            <div className="flex flex-col rounded-lg bg-white/70 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Den richtigen Namen finden
+                                </h2>
 
-            {names.map((name: any, index: number) => {
-                return (
-                    <div key={index} className="card">
-                        <h2>{name.letter}</h2>
+                                <p className="text-sm">
+                                    Auf der Suche nach einem Namen für Dein Baby
+                                    kannst Du Rat in Büchern, Hitlisten oder bei
+                                    einem Namens Generator suchen. Allerdings
+                                    ist es in jedem Fall hilfreich mit ein paar
+                                    Tricks zu arbeiten, um sich langsam an den
+                                    perfekten Namen heranzutasten.
+                                </p>
+                            </div>
 
-                        <div className="flex flex-row flex-wrap">
-                            {name.names.map((item: string, index: number) => {
-                                return (
-                                    <div
-                                        key={index}
-                                        className="flex text-sm w-50"
-                                    >
-                                        {item}
-                                        <span>,&nbsp;</span>
-                                    </div>
-                                )
-                            })}
-                        </div>
+                            <div className="flex flex-col rounded-lg bg-white/70 mt-5 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Listen schreiben
+                                </h2>
+
+                                <p className="text-sm">
+                                    Schreibe alle Ideen auf. Diese Liste wird
+                                    mit der Zeit wachsen, Du wirst Namen
+                                    streichen und hinzufügen, verwerfen oder
+                                    wiederentdecken.
+                                </p>
+                            </div>
+
+                            <div className="flex flex-col rounded-lg bg-white/70 mt-5 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Ohren auf
+                                </h2>
+
+                                <p className="text-sm">
+                                    Inspiration lauert überall. Bleibe
+                                    aufmerksam, denn der perfekte Name könnte in
+                                    Deinem Lieblingsroman stecken oder in einem
+                                    Songtext, eine Person oder ein Ort könnte
+                                    Dich inspirieren.
+                                </p>
+                            </div>
+
+                            <div className="flex flex-col rounded-lg bg-white/70 mt-5 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Aussprechen
+                                </h2>
+
+                                <p className="text-sm">
+                                    Freunde Dich mit dem Klang des Namens an,
+                                    indem Du ihn mehrmals und immer wieder laut
+                                    aussprichst.
+                                </p>
+                            </div>
+
+                            <div className="flex flex-col rounded-lg bg-white/70 mt-5 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Aufschreiben
+                                </h2>
+
+                                <p className="text-sm">
+                                    Überlege Dir auch welche Schreibweise Du für
+                                    den Namen Deines Kindes festlegen willst.
+                                </p>
+                            </div>
+
+                            <div className="flex flex-col rounded-lg bg-white/70 mt-5 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Der Nachname
+                                </h2>
+
+                                <p className="text-sm">
+                                    Achte darauf, dass Vor- und Nachname sich
+                                    flüssig aussprechen lassen.
+                                </p>
+                            </div>
+
+                            <div className="flex flex-col rounded-lg bg-white/70 mt-5 mb-5 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Die Geschwister
+                                </h2>
+
+                                <p className="text-sm">
+                                    Wenn Du möchtest, kannst Du den Namen des
+                                    Babys mit denen der älteren Geschwister
+                                    abstimmen.
+                                </p>
+                            </div>
+                        </TabsContent>
+
+                        <TabsContent value="boy">
+                            <div className="flex flex-col rounded-lg bg-white/70 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Jungennamen
+                                </h2>
+
+                                {amountOfBoyNames && (
+                                    <p className="text-sm">
+                                        Aktuell stehen {amountOfBoyNames.length}{' '}
+                                        Namen zur Auswahl.
+                                    </p>
+                                )}
+                            </div>
+
+                            <NamesList names={boyNames} />
+                        </TabsContent>
+
+                        <TabsContent value="girl">
+                            <div className="flex flex-col rounded-lg bg-white/70 p-4">
+                                <h2 className="text-xl text-blue-500 pb-2">
+                                    Mädchennamen
+                                </h2>
+
+                                {amountOfGirlNames && (
+                                    <p className="text-sm">
+                                        Aktuell stehen{' '}
+                                        {amountOfGirlNames.length} Namen zur
+                                        Auswahl.
+                                    </p>
+                                )}
+                            </div>
+
+                            <NamesList names={girlNames} />
+                        </TabsContent>
                     </div>
-                )
-            })}
+                </div>
+            </Tabs>
         </section>
     )
 }
---
+++ src/components/NamesList.tsx
@@ -0,0 +1,34 @@
+const NamesList = ({ names }: any) => {
+    return (
+        <>
+            {names.map((name: any, index: number) => {
+                return (
+                    <div
+                        key={index}
+                        className="flex flex-col rounded-lg bg-white/70 my-5 p-4"
+                    >
+                        <h2 className="text-xl text-blue-500 pb-2">
+                            {name.letter}
+                        </h2>
+
+                        <div className="flex flex-row flex-wrap">
+                            {name.names.map((item: string, index: number) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        className="flex text-sm w-50"
+                                    >
+                                        {item}
+                                        <span>,&nbsp;</span>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export default NamesList
